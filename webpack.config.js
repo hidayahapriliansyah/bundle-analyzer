cf451585a43252c93541d0f70ba8087c48c48587
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-03-13 13:08:29 +0700

    mengerjakana module latihan:bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
@@ -29,5 +30,6 @@ module.exports = {
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
